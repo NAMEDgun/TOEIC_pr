fe81112f422e2ee2653ea75753f1f15f5d655df1
Chang-Gyeonghyun <ckh0416@ajou.ac.kr>
2024-07-12 19:34:47 +0900

    success connect

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,25 @@
+# main.py (FastAPI 애플리케이션)
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.routers import task
 import uvicorn
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
+
 app.include_router(task.router)
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello World"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
